9f5f1b734b4be350c87aaa3984042e671d8ad881
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-05-21 23:58:32 +0000

    Update colors.py

--- colors.py
+++ colors.py
@@ -3,10 +3,14 @@ class ColorsSet:
 	GREEN = '\033[38;5;82m'
 	YELLOW = '\033[33m'
 	BLUE = '\033[34m'
+	BANNER = '\033[38;5;62m'
+	ORANGE = '\033[38;5;178m'
+	SILVER = '\033[38;5;45m'
 	MAGENTA = '\033[35m'
 	CYAN = '\033[36m'
 	WHITE = '\033[37m'
 	RESET = '\033[0m'
+	PINK = '\033[38;5;164m'
 class Colors:
 	def __init__(self, default_color=None):
 		if default_color:
